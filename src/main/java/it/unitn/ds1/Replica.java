f4008786854a5092b2ccfef5d19eda5b5edd0c2f
luckeez <pluca1609@gmail.com>
2024-09-26 18:21:05 +0200

    initial commit

--- src/main/java/it/unitn/ds1/Replica.java
+++ src/main/java/it/unitn/ds1/Replica.java
@@ -4,6 +4,7 @@ import akka.actor.ActorRef;
 import akka.actor.AbstractActor;
 import akka.actor.Cancellable;
 import akka.actor.Props;
+import ch.qos.logback.core.subst.Token;
 import scala.concurrent.duration.Duration;
 
 import java.io.Serializable;
@@ -30,5 +31,14 @@ public class Replica extends AbstractActor {
     static public Props props(int id){
         return Props.create(Replica.class, () -> new Replica(id));
     }
+
+
+      // Here we define the mapping between the received message types
+  // and our actor methods
+  @Override
+  public Receive createReceive() {
+    return receiveBuilder()
+      .build();
+  }
 
 }
