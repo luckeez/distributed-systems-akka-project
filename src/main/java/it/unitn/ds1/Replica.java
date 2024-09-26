2630521d761fa36c3718c3e7c8bd90a0fcbe0c56
luckeez <pluca1609@gmail.com>
2024-09-26 18:34:59 +0200

    add initial coordinator

--- src/main/java/it/unitn/ds1/Replica.java
+++ src/main/java/it/unitn/ds1/Replica.java
@@ -1,30 +1,63 @@
 package it.unitn.ds1;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
 import akka.actor.AbstractActor;
 import akka.actor.Props;
+import akka.actor.ActorRef;
 
 
 
 public class Replica extends AbstractActor {
     private int id;
     private String v;
+    private List<ActorRef> peers = new ArrayList<>();
+    private boolean isCoordinator;
 
     // CONSTRUCTOR
-    public Replica(int id){
+    public Replica(int id, boolean isCoordinator){
         this.id = id;
+        this.isCoordinator = isCoordinator;
     }
 
-    static public Props props(int id){
-        return Props.create(Replica.class, () -> new Replica(id));
+    static public Props props(int id, boolean isCoordinator){
+        return Props.create(Replica.class, () -> new Replica(id, isCoordinator));
     }
 
 
-      // Here we define the mapping between the received message types
-  // and our actor methods
-  @Override
-  public Receive createReceive() {
-    return receiveBuilder()
-      .build();
-  }
+    /*-- Message classes ------------------------------------------------------ */
+
+    // Start message that informs every participant about its peers
+    public static class JoinGroupMsg implements Serializable {
+        public final List<ActorRef> group;   // an array of group members
+        public JoinGroupMsg(List<ActorRef> group) {
+        this.group = Collections.unmodifiableList(new ArrayList<ActorRef>(group));
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Here we define the mapping between the received message types
+    // and our actor methods
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+        .build();
+    }
 
 }
--- src/main/java/it/unitn/ds1/ReplicaSystem.java
+++ src/main/java/it/unitn/ds1/ReplicaSystem.java
@@ -4,7 +4,10 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 
 import java.util.List;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import it.unitn.ds1.Replica.JoinGroupMsg;
 
 public class ReplicaSystem {
     final static int N = 10;
@@ -13,8 +16,26 @@ public class ReplicaSystem {
         final ActorSystem system = ActorSystem.create("replicasystem");
 
         List<ActorRef> group = new ArrayList<>();
-        for (int i=0;i<N;i++){
-            group.add(system.actorOf(Replica.props(i), "replica" + i));
+
+        // Add initial coordinatoor
+        group.add(system.actorOf(Replica.props(0, true), "replica" + 0));
+
+        // Add other replicas
+        for (int i=1;i<N;i++){
+            group.add(system.actorOf(Replica.props(i, false), "replica" + i));
         }
+
+        
+        JoinGroupMsg start = new JoinGroupMsg(group);
+        for (ActorRef peer : group){
+            peer.tell(start, ActorRef.noSender());
+        }
+
+        try {
+            System.out.println(">>> Press ENTER to exit <<<");
+            System.in.read();
+        }
+        catch (IOException ioe){}
+        system.terminate();
     }
 }
