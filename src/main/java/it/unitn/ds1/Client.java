ebd9c7dce4953dfaee8f473c3a3344d0f9d3cf05
bordi00 <matteo.bordignon@studenti.unitn.it>
2024-10-05 18:20:25 +0200

    bug fix in write request handling added update coordinator logic

--- src/main/java/it/unitn/ds1/Client.java
+++ src/main/java/it/unitn/ds1/Client.java
@@ -5,19 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import javax.swing.AbstractAction;
-
-import java.util.Collections;
-
 import akka.actor.AbstractActor;
 import akka.actor.Props;
 import akka.actor.ActorRef;
 
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 import it.unitn.ds1.Replica.JoinGroupMsg;
-
+import it.unitn.ds1.Replica.ReadResponseMsg;
 
 public class Client extends AbstractActor {
-    private int id;
+    private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+    private final int id;
     private List<ActorRef> replicas = new ArrayList<>();
     private final Random rnd;
 
@@ -51,24 +50,19 @@ public class Client extends AbstractActor {
         }
     }
 
-
     /* ---------------- Client logic ----------------- */
 
     private void sendReadRequest(){
         int to = rnd.nextInt(this.replicas.size());
         replicas.get(to).tell(new ReadRequestMsg(getSelf()), getSelf());
+        log.info("Client {} sent read request to replica {}", this.id, to);
     }
 
     private void sendWriteRequest(int proposedV){
         int to = rnd.nextInt(this.replicas.size());
         replicas.get(to).tell(new WriteRequestMsg(getSelf(), proposedV), getSelf());
+        log.info("Client {} sent write request to replica {} with value {}", this.id, to, proposedV);
     }
-
-
-
-
-
-
 
     private void onJoinGroupMsg(JoinGroupMsg msg){
         for (ActorRef a : msg.group){
@@ -76,11 +70,12 @@ public class Client extends AbstractActor {
                 this.replicas.add(a);  // copy all replicas except for self
             }
         }
+        log.info("Client {} joined group with {} replicas", this.id, this.replicas.size());
     }
 
-
-
-
+    private void onReadResponseMsg(ReadResponseMsg msg){
+        log.info("Client {} received value: {}", this.id, msg.v);
+    }
 
     /* --------------------------------------------------------- */
 
@@ -90,10 +85,7 @@ public class Client extends AbstractActor {
     public Receive createReceive() {
         return receiveBuilder()
             .match(JoinGroupMsg.class, this::onJoinGroupMsg)
-        .build();
+            .match(ReadResponseMsg.class, this::onReadResponseMsg)
+            .build();
     }
-
-
-
-
 }
--- src/main/java/it/unitn/ds1/ReplicaSystem.java
+++ src/main/java/it/unitn/ds1/ReplicaSystem.java
@@ -2,14 +2,19 @@ package it.unitn.ds1;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 
-import java.util.List;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import it.unitn.ds1.Replica.JoinGroupMsg;
+import it.unitn.ds1.Client.ReadRequestMsg;
+import it.unitn.ds1.Client.WriteRequestMsg;
 
 public class ReplicaSystem {
+    private static final LoggingAdapter log = Logging.getLogger(ActorSystem.create("replicasystem"), ReplicaSystem.class);
     final static int N = 10;
 
     public static void main(String[] args) {
@@ -17,25 +22,43 @@ public class ReplicaSystem {
 
         List<ActorRef> group = new ArrayList<>();
 
-        // Add initial coordinatoor
-        group.add(system.actorOf(Replica.props(0, true), "replica" + 0));
+        ActorRef initialCoordinator = system.actorOf(Replica.props(0, true, null), "replica0");
+        group.add(initialCoordinator);
+        log.info("Added initial coordinator: replica0");
 
         // Add other replicas
-        for (int i=1;i<N;i++){
-            group.add(system.actorOf(Replica.props(i, false), "replica" + i));
+        for (int i = 1; i < N; i++) {
+            group.add(system.actorOf(Replica.props(i, false, initialCoordinator), "replica" + i));
+            log.info("Added replica: replica{}", i);
         }
 
-        
         JoinGroupMsg start = new JoinGroupMsg(group);
-        for (ActorRef peer : group){
+        for (ActorRef peer : group) {
             peer.tell(start, ActorRef.noSender());
+            log.info("Sent JoinGroupMsg to: {}", peer.path().name());
         }
+
+        // Create a client
+        ActorRef client = system.actorOf(Client.props(1), "client1");
+        log.info("Created client: client1");
+
+        // Send read and write requests
+        // group.get(1).tell(new ReadRequestMsg(client), ActorRef.noSender());
+        // log.info("Sent ReadRequestMsg from client1");
+        // TODO: fix ReadRequestMsg hadling in Client
+
+
+        group.get(1).tell(new WriteRequestMsg(client, 42), ActorRef.noSender());
+        log.info("Sent WriteRequestMsg from client1 with value 42");
 
         try {
-            System.out.println(">>> Press ENTER to exit <<<");
+            log.info(">>> Press ENTER to exit <<<");
             System.in.read();
+        } catch (IOException ioe) {
+            log.error("IOException occurred", ioe);
+        } finally {
+            system.terminate();
+            log.info("Actor system terminated");
         }
-        catch (IOException ioe){}
-        system.terminate();
     }
 }
